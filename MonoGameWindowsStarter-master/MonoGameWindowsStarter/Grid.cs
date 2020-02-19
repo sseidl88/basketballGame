c3b10356a8915c9a72578b465b147465477661b0
sseidl88 <samueleseidl@gmail.com>
2020-02-19 17:31:01 -0600

    Added grid

---
+++ MonoGameWindowsStarter-master/MonoGameWindowsStarter/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+namespace MonoGameWindowsStarter
+{
+    public class Grid
+    {
+        Game1 game;
+        int NUM_CELLS;
+        int CELL_SIZE_x ;
+        int CELL_SIZE_y ;
+        public Grid(Game1 game)
+        {
+            this.game = game;
+
+            //clear the grid
+            for(int x = 0; x < NUM_CELLS; x++)
+            {
+                for(int y = 0; y < NUM_CELLS; y++)
+                {
+                }
+            }
+
+        }
+    }
+}
